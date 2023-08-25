982f30160c31221f20b75fc9420e531e05fee598
yuseonjun <seonjun0906@gmail.com>
2023-08-25 14:37:49 +0900

    feat: 다리 길이를 입력받는 메소드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,26 @@
 package bridge.view;
 
+import static bridge.view.InputValidator.validateBridgeSize;
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_MESSAGE = "다리의 길이를 입력해주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(BRIDGE_GAME_START_MESSAGE);
+        System.out.println(INPUT_BRIDGE_MESSAGE);
+        String inputBridgeSize = Console.readLine();
+        validateBridgeSize(inputBridgeSize);
+        return Integer.parseInt(inputBridgeSize);
     }
 
     /**
