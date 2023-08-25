5c16cef21e88557923440b312ec54b77abd32134
yuseonjun <seonjun0906@gmail.com>
2023-08-25 18:24:36 +0900

    feat: 추가적인 예외 검증 구현

--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -7,7 +7,10 @@ public class InputValidator {
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String INVALID_INPUT_BRIDGE_NUMBER = " 숫자 형식이 아닙니다.";
     private static final String INVALID_BLANK = " 값을 입력하세요.";
-    public static final String INVALID_INPUT_BRIDGE_STRING = " 문자 형식이 아닙니다.";
+    private static final String INVALID_INPUT_BRIDGE_STRING = " 문자 형식이 아닙니다.";
+    private static final String INVALID_BRIDGE_SIZE = " 3에서 20사이의 숫자를 입력해주세요.";
+    private static final String INVALID_MOVE_DIRECTION = " U와 D중 하나의 값을 입력해주세요.";
+    private static final String INVALID_RETRY_COMMAND = " R과 Q중 하나의 값을 입력해주세요.";
 
     private InputValidator() {
     }
@@ -15,24 +18,26 @@ public class InputValidator {
     public static void validateBridgeSize(String inputBridgeSize) {
         validateBlank(inputBridgeSize);
         Integer number = checkInteger(inputBridgeSize);
+        checkInputBridgeSize(number);
     }
 
     public static void validateBridgeMove(String inputBridgeMove) {
         validateBlank(inputBridgeMove);
         checkString(inputBridgeMove);
+        checkInputMove(inputBridgeMove);
     }
 
     public static void validateBridgeCommand(String inputBridgeRetryCommand) {
         validateBlank(inputBridgeRetryCommand);
         checkString(inputBridgeRetryCommand);
+        checkInputRetryCommand(inputBridgeRetryCommand);
     }
 
     private static Integer checkInteger(String input) {
         try {
             return Integer.valueOf(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(
-                ERROR_MESSAGE + INVALID_INPUT_BRIDGE_NUMBER);
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_INPUT_BRIDGE_NUMBER);
         }
     }
 
@@ -44,8 +49,25 @@ public class InputValidator {
 
     private static void checkString(String input) {
         if (!Pattern.matches("^[a-zA-Z]*$", input)) {
-            throw new IllegalArgumentException(
-                ERROR_MESSAGE + INVALID_INPUT_BRIDGE_STRING);
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_INPUT_BRIDGE_STRING);
+        }
+    }
+
+    private static void checkInputBridgeSize(int bridgeSize) {
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_BRIDGE_SIZE);
+        }
+    }
+
+    private static void checkInputMove(String move) {
+        if (!(move.equals("U") || move.equals("R"))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_MOVE_DIRECTION);
+        }
+    }
+
+    private static void checkInputRetryCommand(String command) {
+        if (!(command.equals("R") || command.equals("Q"))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_RETRY_COMMAND);
         }
     }
 }
