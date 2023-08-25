b07d27446d97ef5d158926b643076eec4d957b40
yuseonjun <seonjun0906@gmail.com>
2023-08-25 14:53:49 +0900

    feat: 사용자의 움직입을 입력하는 메소드 구현

--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -7,6 +7,7 @@ public class InputValidator {
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String INVALID_INPUT_BRIDGE_NUMBER = " 숫자 형식이 아닙니다.";
     private static final String INVALID_BLANK = " 값을 입력하세요.";
+    public static final String INVALID_INPUT_BRIDGE_STRING = " 문자 형식이 아닙니다.";
 
     private InputValidator() {
     }
@@ -14,6 +15,11 @@ public class InputValidator {
     public static void validateBridgeSize(String inputBridgeSize) {
         validateBlank(inputBridgeSize);
         Integer number = checkInteger(inputBridgeSize);
+    }
+
+    public static void validateBridgeMove(String inputBridgeMove) {
+        validateBlank(inputBridgeMove);
+        checkString(inputBridgeMove);
     }
 
     private static Integer checkInteger(String input) {
@@ -28,6 +34,13 @@ public class InputValidator {
     private static void validateBlank(String number) {
         if (number.isBlank()) {
             throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_BLANK);
+        }
+    }
+
+    private static void checkString(String input) {
+        if (!Pattern.matches("^[a-zA-Z]*$", input)) {
+            throw new IllegalArgumentException(
+                ERROR_MESSAGE + INVALID_INPUT_BRIDGE_STRING);
         }
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import static bridge.view.InputValidator.validateBridgeMove;
 import static bridge.view.InputValidator.validateBridgeSize;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -11,6 +12,7 @@ public class InputView {
 
     private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String INPUT_BRIDGE_MESSAGE = "다리의 길이를 입력해주세요.";
+    public static final String INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -27,7 +29,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVE_MESSAGE);
+        String inputBridgeMove = Console.readLine();
+        validateBridgeMove(inputBridgeMove);
+        return inputBridgeMove;
     }
 
     /**
