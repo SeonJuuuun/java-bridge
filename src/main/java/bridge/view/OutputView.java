6354b6eed01c39072b9935fe571cb51278acde23
yuseonjun <seonjun0906@gmail.com>
2023-08-25 17:51:10 +0900

    feat: 맵을 만드는 메서드 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,9 @@
 package bridge.view;
 
+import bridge.BridgeGame;
+import bridge.domain.MoveDirection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -25,5 +28,19 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+
+    }
+
+    private Map<MoveDirection, List<String>> generateMap(BridgeGame bridgeGame) {
+        int round = bridgeGame.getRound();
+        List<String> bridge = bridgeGame.getBridge();
+        Map<MoveDirection, List<String>> map = initMap();
+
+        for (int i = 0; i < round - 1; i++) {
+            cross(map, MoveDirection.of(bridge.get(i)), true);
+        }
+
+        cross(map, MoveDirection.of(bridge.get(round - 1)), bridgeGame.isSuccess());
+        return map;
     }
 }
