28ef7626ab070c3a986eb4698c3d2fc2c9bfec75
yuseonjun <seonjun0906@gmail.com>
2023-08-25 17:51:56 +0900

    feat: 최종 출력 문구와 맵 값을 넣어주는 initMap 메서드 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,6 +2,9 @@ package bridge.view;
 
 import bridge.BridgeGame;
 import bridge.domain.MoveDirection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +18,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(BridgeGame bridgeGame) {
+        Map<MoveDirection, List<String>> map = generateMap(bridgeGame);
+        printRow(map.get(MoveDirection.UP));
+        printRow(map.get(MoveDirection.DOWN));
     }
 
     private void printRow(List<String> row) {
@@ -27,9 +33,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-
+    public void printResult(boolean success, int trialCount, BridgeGame bridgeGame) {
+        System.out.println("최종 게임 결과");
+        printMap(bridgeGame);
+        System.out.println("게임 성공 여부: " + (success ? "성공" : "실패"));
+        System.out.println("총 시도한 횟수: " + trialCount);
     }
+
 
     private Map<MoveDirection, List<String>> generateMap(BridgeGame bridgeGame) {
         int round = bridgeGame.getRound();
@@ -41,6 +51,14 @@ public class OutputView {
         }
 
         cross(map, MoveDirection.of(bridge.get(round - 1)), bridgeGame.isSuccess());
+        return map;
+    }
+
+    private Map<MoveDirection, List<String>> initMap() {
+        Map<MoveDirection, List<String>> map = new HashMap<>();
+        for (MoveDirection direction : MoveDirection.values()) {
+            map.put(direction, new ArrayList<>());
+        }
         return map;
     }
 }
