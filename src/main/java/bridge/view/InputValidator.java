0dab3e9b992cd15511f27adf92cf9269dde15a91
yuseonjun <seonjun0906@gmail.com>
2023-08-26 17:04:29 +0900

    refactor: 상수화 처리

--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -12,6 +12,13 @@ public class InputValidator {
     private static final String INVALID_BRIDGE_SIZE = " 3에서 20사이의 숫자를 입력해주세요.";
     private static final String INVALID_MOVE_DIRECTION = " U와 D중 하나의 값을 입력해주세요.";
     private static final String INVALID_RETRY_COMMAND = " R과 Q중 하나의 값을 입력해주세요.";
+    private static final String MOVE_U = "U";
+    private static final String MOVE_D = "D";
+    private static final String COMMAND_R = "R";
+    private static final String COMMAND_Q = "Q";
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final int MAX_BRIDGE_SIZE = 20;
+
 
     private InputValidator() {
     }
@@ -55,19 +62,19 @@ public class InputValidator {
     }
 
     private static void checkInputBridgeSize(int bridgeSize) {
-        if (bridgeSize < 3 || bridgeSize > 20) {
+        if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
             throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_BRIDGE_SIZE);
         }
     }
 
     private static void checkInputMove(String move) {
-        if (!(move.equals("U") || move.equals("D"))) {
+        if (!(move.equals(MOVE_U) || move.equals(MOVE_D))) {
             throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_MOVE_DIRECTION);
         }
     }
 
     private static void checkInputRetryCommand(String command) {
-        if (!(command.equals("R") || command.equals("Q"))) {
+        if (!(command.equals(COMMAND_R) || command.equals(COMMAND_Q))) {
             throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_RETRY_COMMAND);
         }
     }
